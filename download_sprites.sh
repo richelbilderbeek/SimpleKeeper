59935ce3b2821fce63b8895313f5a5c6928ba587
richelbilderbeek <richel@richelbilderbeek.nl>
2016-04-24 15:30:21 +0200

    All three Links

--- download_sprites.sh
+++ download_sprites.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+
+# Red
+wget http://www.videogamesprites.net/Zelda1/Link/Link%20%28Red%29%20%28Front%291.gif -O LinkRedFrontSmallShield.gif
+wget http://www.videogamesprites.net/Zelda1/Link/Link%20%28Red%29%20%28Front%292.gif -O LinkRedFrontBigShield.gif
+wget http://www.videogamesprites.net/Zelda1/Link/Link%20%28Red%29%20%28Left%291.gif -O LinkRedLeftSmallShield.gif
+wget http://www.videogamesprites.net/Zelda1/Link/Link%20%28Red%29%20%28Left%292.gif -O LinkRedLeftBigShield.gif
+wget http://www.videogamesprites.net/Zelda1/Link/Link%20%28Red%29%20%28Back%29.gif -O LinkRedBack.gif
+
+convert  "LinkRedFrontSmallShield.gif[0]" LinkRedFrontSmallShield1.png
+convert  "LinkRedFrontSmallShield.gif[1]" LinkRedFrontSmallShield2.png
+convert  "LinkRedLeftSmallShield.gif[0]" LinkRedLeftSmallShield1.png
+convert  "LinkRedLeftSmallShield.gif[1]" LinkRedLeftSmallShield2.png
+convert  "LinkRedBack.gif[0]" LinkRedBack1.png
+convert  "LinkRedBack.gif[1]" LinkRedBack2.png
+
+# Green
 wget http://www.videogamesprites.net/Zelda1/Link/Link%20%28Normal%29%20%28Front%291.gif -O LinkGreenFrontSmallShield.gif
 wget http://www.videogamesprites.net/Zelda1/Link/Link%20%28Normal%29%20%28Front%292.gif -O LinkGreenFrontBigShield.gif
 wget http://www.videogamesprites.net/Zelda1/Link/Link%20%28Normal%29%20%28Left%291.gif -O LinkGreenLeftSmallShield.gif
@@ -11,3 +27,17 @@ convert  "LinkGreenLeftSmallShield.gif[0]" LinkGreenLeftSmallShield1.png
 convert  "LinkGreenLeftSmallShield.gif[1]" LinkGreenLeftSmallShield2.png
 convert  "LinkGreenBack.gif[0]" LinkGreenBack1.png
 convert  "LinkGreenBack.gif[1]" LinkGreenBack2.png
+
+# Blue
+wget http://www.videogamesprites.net/Zelda1/Link/Link%20%28Blue%29%20%28Front%291.gif -O LinkBlueFrontSmallShield.gif
+wget http://www.videogamesprites.net/Zelda1/Link/Link%20%28Blue%29%20%28Front%292.gif -O LinkBlueFrontBigShield.gif
+wget http://www.videogamesprites.net/Zelda1/Link/Link%20%28Blue%29%20%28Left%291.gif -O LinkBlueLeftSmallShield.gif
+wget http://www.videogamesprites.net/Zelda1/Link/Link%20%28Blue%29%20%28Left%292.gif -O LinkBlueLeftBigShield.gif
+wget http://www.videogamesprites.net/Zelda1/Link/Link%20%28Blue%29%20%28Back%29.gif -O LinkBlueBack.gif
+
+convert  "LinkBlueFrontSmallShield.gif[0]" LinkBlueFrontSmallShield1.png
+convert  "LinkBlueFrontSmallShield.gif[1]" LinkBlueFrontSmallShield2.png
+convert  "LinkBlueLeftSmallShield.gif[0]" LinkBlueLeftSmallShield1.png
+convert  "LinkBlueLeftSmallShield.gif[1]" LinkBlueLeftSmallShield2.png
+convert  "LinkBlueBack.gif[0]" LinkBlueBack1.png
+convert  "LinkBlueBack.gif[1]" LinkBlueBack2.png
