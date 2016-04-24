0be78c8fdfc831dd51e54f03013fc49fa69ec23b
richelbilderbeek <richel@richelbilderbeek.nl>
2016-04-24 15:27:16 +0200

    Only green Link

--- download_sprites.sh
+++ download_sprites.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-wget http://www.videogamesprites.net/Zelda1/Link/Link%20%28Normal%29%20%28Front%291.gif -O LinkFrontSmallShield.gif
-wget http://www.videogamesprites.net/Zelda1/Link/Link%20%28Normal%29%20%28Front%292.gif -O LinkFrontBigShield.gif
-wget http://www.videogamesprites.net/Zelda1/Link/Link%20%28Normal%29%20%28Left%291.gif -O LinkLeftSmallShield.gif
-wget http://www.videogamesprites.net/Zelda1/Link/Link%20%28Normal%29%20%28Left%292.gif -O LinkLeftBigShield.gif
-wget http://www.videogamesprites.net/Zelda1/Link/Link%20%28Normal%29%20%28Back%29.gif -O LinkBack.gif
+wget http://www.videogamesprites.net/Zelda1/Link/Link%20%28Normal%29%20%28Front%291.gif -O LinkGreenFrontSmallShield.gif
+wget http://www.videogamesprites.net/Zelda1/Link/Link%20%28Normal%29%20%28Front%292.gif -O LinkGreenFrontBigShield.gif
+wget http://www.videogamesprites.net/Zelda1/Link/Link%20%28Normal%29%20%28Left%291.gif -O LinkGreenLeftSmallShield.gif
+wget http://www.videogamesprites.net/Zelda1/Link/Link%20%28Normal%29%20%28Left%292.gif -O LinkGreenLeftBigShield.gif
+wget http://www.videogamesprites.net/Zelda1/Link/Link%20%28Normal%29%20%28Back%29.gif -O LinkGreenBack.gif
 
-convert  "LinkFrontSmallShield.gif[0]" LinkFrontSmallShield1.png
-convert  "LinkFrontSmallShield.gif[1]" LinkFrontSmallShield2.png
-convert  "LinkLeftSmallShield.gif[0]" LinkLeftSmallShield1.png
-convert  "LinkLeftSmallShield.gif[1]" LinkLeftSmallShield2.png
-convert  "LinkBack.gif[0]" LinkBack1.png
-convert  "LinkBack.gif[1]" LinkBack2.png
+convert  "LinkGreenFrontSmallShield.gif[0]" LinkGreenFrontSmallShield1.png
+convert  "LinkGreenFrontSmallShield.gif[1]" LinkGreenFrontSmallShield2.png
+convert  "LinkGreenLeftSmallShield.gif[0]" LinkGreenLeftSmallShield1.png
+convert  "LinkGreenLeftSmallShield.gif[1]" LinkGreenLeftSmallShield2.png
+convert  "LinkGreenBack.gif[0]" LinkGreenBack1.png
+convert  "LinkGreenBack.gif[1]" LinkGreenBack2.png
